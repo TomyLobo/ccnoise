f19b2ec286c423f1b941d1bc3da291e8d5ff02de
TomyLobo <tomylobo@nurfuerspam.de>
2011-12-25 23:26:02 +0100

    Added "and contributors" to all Copyright statements on the com.sk89q.worldedit.expression package.

--- src/eu/tomylobo/expression/lexer/tokens/KeywordToken.java
+++ src/eu/tomylobo/expression/lexer/tokens/KeywordToken.java
@@ -1,6 +1,6 @@
 /*
  * Expression Parser
- * Copyright (C) 2010, 2011 sk89q <http://www.sk89q.com>
+ * Copyright (C) 2011, 2012, 2013 TomyLobo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
