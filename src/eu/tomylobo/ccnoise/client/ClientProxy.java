f18790378e448a26659a50a48405e896b53fb66e
TomyLobo <tomylobo@nurfuerspam.de>
2013-05-02 18:52:42 +0200

    Added CommonProxy.debugPrint.

--- src/eu/tomylobo/ccnoise/client/ClientProxy.java
+++ src/eu/tomylobo/ccnoise/client/ClientProxy.java
@@ -1,5 +1,7 @@
 package eu.tomylobo.ccnoise.client;
 
+import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.MinecraftForgeClient;
 import eu.tomylobo.ccnoise.common.CommonProxy;
 
@@ -7,5 +9,16 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void init() {
 		MinecraftForgeClient.preloadTexture(TEX_BLOCKS);
+	}
+
+	@Override
+	public void debugPrint(String format, Object... args) {
+		super.debugPrint(format, args);
+
+		Minecraft mc = FMLClientHandler.instance().getClient();
+		if (mc.ingameGUI != null)
+		{
+			mc.ingameGUI.getChatGUI().printChatMessage(String.format(format, args));
+		}
 	}
 }
--- src/eu/tomylobo/ccnoise/common/CommonProxy.java
+++ src/eu/tomylobo/ccnoise/common/CommonProxy.java
@@ -5,4 +5,8 @@ public class CommonProxy {
 
 	public void init() {
 	}
+
+	public void debugPrint(String format, Object... args) {
+		System.out.println(String.format(format, args));
+	}
 }
